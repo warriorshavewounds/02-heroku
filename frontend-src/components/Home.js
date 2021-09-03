5ec9d7fd628ff3570b831bf4d3a3638f9148a8b6
Emmanuel Yaw Amoako <nanaamoako919@yahoo.co.uk>
2021-09-04 00:12:09 +0100

    attempt2 add image

--- frontend-src/components/Home.js
+++ frontend-src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div>
       <h2 className="display-4">Welcome: CAG homepage!</h2>
-      <img className="img-fluid" src='image\cag.jpg' />
+      <img className="photo" src='image\cag.jpg' />
       <p className="lead">A seed Grows With No Sound. Creation is Quiet.</p>
       <p>So work hard in silence, let your success be your noise.
         
